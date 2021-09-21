94fa49625cb9ee72c05a77dd0f53a4e440fad6bd
Emrecan Ay <codemrecan@gmail.com>
2021-09-21 23:36:34 +0300

    Day 8 done!

--- src/main/java/kodlamaio/hrms/business/abstracts/JobAdvertisementService.java
+++ src/main/java/kodlamaio/hrms/business/abstracts/JobAdvertisementService.java
@@ -8,10 +8,19 @@ import kodlamaio.hrms.entites.concretes.JobAdvertisement;
 import kodlamaio.hrms.entites.dtos.JobAdvertisementDto;
 
 public interface JobAdvertisementService {
-
+	
 	DataResult<List<JobAdvertisement>> getall();
 	Result add(JobAdvertisement jobAdvertisement);
 	
+	DataResult<List<JobAdvertisement>> getActiveJobs();
+
+	DataResult<List<JobAdvertisement>> getAllSortedByCreatedDate();
+	
 	DataResult<List<JobAdvertisementDto>> jobAdvertisementDetails();
+
+	DataResult<List<JobAdvertisement>> getByCompanyAndActiveJobs(String companyName);
+	
+	Result setIsActive(int id);
+	
 	
 }
